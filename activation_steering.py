5951837603c2e1e14c196432e746ac05996f16f6
Joris Postmus <jorispostmus@gmail.com>
2023-11-17 15:39:53 +0100

    integrated conceptors

--- activation_steering.py
+++ activation_steering.py
@@ -19,48 +19,60 @@ TARGET_LAYER = 6  # Target layer to apply conceptor
 _torch_module_call = torch.nn.Module.__call__
 
 class LayerTracker:
+    """Tracks the current layer during the forward pass of the model."""
     def __init__(self) -> None:
         self.current_layer = 0  # Initialize to 0
 
 def module_forward_wrapper(conceptors: Dict[int, torch.Tensor], layer_tracker: LayerTracker) -> Callable[..., Any]:
+    """
+    Wraps the forward pass of a module to apply conceptors at a specified layer.
+    Args:
+        conceptors (Dict[int, torch.Tensor]): Dictionary of conceptors for each layer.
+        layer_tracker (LayerTracker): Tracker to keep track of the current layer.
+    Returns:
+        Callable[..., Any]: Wrapped forward function.
+    """
     def my_forward(mod: nn.Module, *args, **kwargs) -> Any:
         out = _torch_module_call(mod, *args, **kwargs)
-
         if isinstance(mod, GPT2Block):
             if layer_tracker.current_layer == TARGET_LAYER:
                 c = conceptors.get(layer_tracker.current_layer, None)
                 if c is not None:
-                    # Assuming the first element of the tuple is the activations
                     activations = out[0]
-                    # Apply the conceptor
                     new_activations = torch.matmul(activations, c)
-                    # Reassemble the output tuple
                     out = (new_activations,) + out[1:]
-
             layer_tracker.current_layer += 1
-
         return out
-
     return my_forward
 
-
 class ConceptorSteering:
-    """Context manager to apply conceptors in the forward pass."""
-
+    """
+    Context manager to apply conceptors in the forward pass of the GPT-2 model.
+    """
     def __init__(self, mod: nn.Module, c: Dict[int, torch.Tensor]) -> None:
         self.original_torch_call = nn.Module.__call__
         self.conceptors = c
         self.layer_tracker = LayerTracker()
 
     def __enter__(self) -> "ConceptorSteering":
-        # Override the torch call method
         nn.Module.__call__ = module_forward_wrapper(self.conceptors, self.layer_tracker)
         return self
 
     def __exit__(self, exc_type, exc_value, exc_tb):
-        # Restore normal behavior
         nn.Module.__call__ = self.original_torch_call
 
+def load_conceptors(file_path, target_layer):
+    """
+    Load the conceptor matrices from a file and extract the matrix for the target layer.
+    Args:
+        file_path (str): Path to the file containing the conceptor matrices.
+        target_layer (int): The target layer index for which the conceptor is needed.
+    Returns:
+        torch.Tensor: The conceptor matrix for the specified layer.
+    """
+    conceptor_matrices = np.load(file_path)
+    conceptor_matrix = conceptor_matrices[target_layer]
+    return torch.tensor(conceptor_matrix, dtype=torch.float32)
 
 def process_prompt(model, tokenizer, text, device, conceptors=None, max_length=50, use_steering=True):
     """
@@ -77,21 +89,24 @@ def process_prompt(model, tokenizer, text, device, conceptors=None, max_length=5
     input_ids = tokenizer.encode(text, return_tensors='pt').to(device)
     model.eval()
 
-    generated_text = []
     steering_context = ConceptorSteering(model, conceptors) if use_steering and conceptors else None
     with torch.no_grad():
         if steering_context:
             with steering_context:
-                generated_text = _generate_text(model, input_ids, max_length, tokenizer)
+                return _generate_text(model, input_ids, max_length, tokenizer)
         else:
-            generated_text = _generate_text(model, input_ids, max_length, tokenizer)
-
-    return generated_text
-
+            return _generate_text(model, input_ids, max_length, tokenizer)
 
 def _generate_text(model, input_ids, max_length, tokenizer):
     """
     Helper function to generate text auto-regressively.
+    Args:
+        model (torch.nn.Module): The GPT-2 model used for text generation.
+        input_ids (torch.Tensor): Tensor of input token IDs for the prompt.
+        max_length (int): The maximum length of the text to generate in tokens.
+        tokenizer (transformers.PreTrainedTokenizer): The tokenizer for GPT-2.
+    Returns:
+        str: The generated text as a single string.
     """
     generated_text = []
     for _ in range(max_length):
@@ -99,29 +114,27 @@ def _generate_text(model, input_ids, max_length, tokenizer):
         next_token_logits = outputs.logits[:, -1, :]
         next_token = torch.argmax(next_token_logits, dim=-1)
         input_ids = torch.cat([input_ids, next_token.unsqueeze(0)], dim=-1)
-
         generated_text.append(next_token.item())
         if next_token.item() == tokenizer.eos_token_id:
             break
-
     return tokenizer.decode(generated_text, skip_special_tokens=True)
-
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     tokenizer = GPT2Tokenizer.from_pretrained(MODEL_NAME)
     model = GPT2LMHeadModel.from_pretrained(MODEL_NAME).to(device)
 
-    # Initialize the identity matrix conceptor for the target layer
-    identity_matrix = torch.eye(1600)  # Adjust as needed
-    conceptors = {TARGET_LAYER: identity_matrix}
+    # Load the conceptor matrix for the target layer
+    conceptor_file_path = "activationsData/conceptors.npy"
+    conceptor_matrix = load_conceptors(conceptor_file_path, TARGET_LAYER)
+    conceptors = {TARGET_LAYER: conceptor_matrix}
 
-    prompt = "This research project is about"
+    prompt = "The man wore his suit today and the woman wore her dress, because they"
     print("Prompt:\n", prompt)
 
     # Generate steered output
     steered_output = process_prompt(model, tokenizer, prompt, device, conceptors, max_length=50, use_steering=True)
-    print("------------------------------\nSteered Completion: (identity matrix for now)\n", steered_output)
+    print("------------------------------\nSteered Completion:\n", steered_output)
 
     # Generate unsteered output
     unsteered_output = process_prompt(model, tokenizer, prompt, device, max_length=50, use_steering=False)
